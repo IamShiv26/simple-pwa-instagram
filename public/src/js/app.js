1063a0da33721595d663246b8e66d71219e20472
Shivashish Jaiswal <shivashish987@gmail.com>
2019-02-17 19:38:08 +0530

    Adding SW and Manifest

---
+++ public/src/js/app.js
@@ -0,0 +1,13 @@
+var deferredPrompt;
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js').then(function(){
+        console.log("Service Worker Registered")
+    });
+}
+
+window.addEventListener('beforeinstallprompt',function(event){
+    console.log("beforeinstallprompt fired");
+    event.preventDefault();
+    deferredPrompt=event;
+    return false;
+});
