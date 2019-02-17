abc0f7379e316dd44548042f4de79665c99f18d3
Shivashish Jaiswal <shivashish987@gmail.com>
2019-02-18 02:07:13 +0530

    Adding Service Worker Caching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -19,43 +19,43 @@ window.addEventListener('beforeinstallprompt',function(event){
     return false;
 });
 
-//GET Request using Tradition AJAx (Synchronous) Cannot be used in SWs
-var xhr = new XMLHttpRequest();
-xhr.open("GET","https://httpbin.org/ip");
-xhr.responseType="json";
-xhr.onload = function(){
-    console.log(xhr.response);
-};
-xhr.onerror = function(){
-    console.log("Error!");
-};
-xhr.send();
+// //GET Request using Tradition AJAx (Synchronous) Cannot be used in SWs
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET","https://httpbin.org/ip");
+// xhr.responseType="json";
+// xhr.onload = function(){
+//     console.log(xhr.response);
+// };
+// xhr.onerror = function(){
+//     console.log("Error!");
+// };
+// xhr.send();
 
-//Get Request Using Fetch API (Asynchronous) Used in SW
-fetch("https://httpbin.org/ip").then(function(response){
-    console.log(response);
-    return response.json();
-}).then(function(data){
-    console.log(data);
-}).catch(function(err){
-    console.log(err);
-});
+// //Get Request Using Fetch API (Asynchronous) Used in SW
+// fetch("https://httpbin.org/ip").then(function(response){
+//     console.log(response);
+//     return response.json();
+// }).then(function(data){
+//     console.log(data);
+// }).catch(function(err){
+//     console.log(err);
+// });
 
-fetch("https://httpbin.org/post",{
-    method:"POST",
-    headers:{
-        "Content-Type":"application-json",
-        "Accept":"application-json"
-    },
-    mode:"cors",//use "no-cors" if Access-Control-Allow-Origin error occurs
-    body:JSON.stringify({
-        message:"Does this work?"
-    })
-}).then(function(response){
-    console.log(response);
-    return response.json();
-}).then(function(data){
-    console.log(data);
-}).catch(function(err){
-    console.log(err);
-});
+// fetch("https://httpbin.org/post",{
+//     method:"POST",
+//     headers:{
+//         "Content-Type":"application-json",
+//         "Accept":"application-json"
+//     },
+//     mode:"cors",//use "no-cors" if Access-Control-Allow-Origin error occurs
+//     body:JSON.stringify({
+//         message:"Does this work?"
+//     })
+// }).then(function(response){
+//     console.log(response);
+//     return response.json();
+// }).then(function(data){
+//     console.log(data);
+// }).catch(function(err){
+//     console.log(err);
+// });
